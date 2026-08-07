ee1661ade0ceba0019abb1ee84a508af62aabb3c
NEONN1111 <totallymaplebackup@gmail.com>
2026-08-07 09:45:01 -0400

    fixes to armor parent hullmod and templar midright section sprite

--- src/neon/xdp/data/scripts/util/ArmorParent.kt
+++ src/neon/xdp/data/scripts/util/ArmorParent.kt
@@ -9,6 +9,7 @@ import com.fs.starfarer.api.combat.listeners.AdvanceableListener
 import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI
 import com.fs.starfarer.api.combat.listeners.DamageListener
 import com.fs.starfarer.api.combat.listeners.HullDamageAboutToBeTakenListener
+import com.fs.starfarer.api.input.InputEventAPI
 import org.lwjgl.util.vector.Vector2f
 
 const val MODULE_DEAD = "module_dead"
@@ -34,17 +35,9 @@ class ArmorParent: BaseHullMod() {
     class ArmorModuleChild(val module: ShipAPI): DamageListener, HullDamageAboutToBeTakenListener, AdvanceableListener {
         override fun advance(amount: Float) {
             val engine = Global.getCombatEngine()
-            // return if we are sure the module is no longer in play
+
             if (Global.getCurrentState() != GameState.COMBAT || engine == null || !Global.getCombatEngine().isEntityInPlay(module) ||
                 module.parentStation?.isAlive != true || module.hitpoints <= 0 || module.hasTag(MODULE_DEAD)) return
-
-            /*
-            Enemy AI prioritizes targeting modules over the base hull. While logical for ship/station sections, this creates issues with armor modules.
-            To prevent this, I set the module to a 'hulk' state, causing the AI to ignore it.
-            However, this triggers a distracting visual whiteout ship explosion the first time it is done.
-            My solution is to teleport the module offscreen (currently, a map corner) before marking it as a hulk.
-            This must be done after the ship is loaded into the map and within its borders to prevent the game from despawning it.
-            */
 
             val pad = 50f
             val moduleInMap = (module.location.x in (pad - engine.mapWidth / 2)..(engine.mapWidth / 2 - pad)) &&
@@ -56,23 +49,16 @@ class ArmorParent: BaseHullMod() {
                 val borderEdgeY = if (module.location.getY() > 0) engine.mapHeight / 2 else -engine.mapHeight / 2
 
                 module.location.set(borderEdgeX, borderEdgeY)
-                module.isHulk = true
 
-                /*
-                I set the modules to be station drones, this makes the enemy AI not see the ship as a group of ships. (ie: a capital with 4 frigate escorts)
-                Without this enemies AI would not try to fight module ships as they think it is a many vs 1.
-                This can also be avoided by setting the module hullsize to be a fighter, but that has other rendering issues. (fighters always render over everything else)
-                */
                 module.isDrone = true
                 module.addTag(MODULE_HULKED)
 
-                // set module captain to parent captain, no clue if this is already done or not, but best be safe.
                 module.captain = module.parentStation.captain
             }
 
-            // re-set the module to be a hulk if it's not, this happens after hulk is unset for vanilla damage fx's
-            if (!module.isHulk && module.hasTag(MODULE_HULKED)) {
-                module.isHulk = true
+
+            if (module.hasTag(MODULE_HULKED) && !module.isDrone && !module.hasTag(MODULE_DEAD)) {
+                module.isDrone = true
             }
 
             // sync hardflux level with parent hull for polarized armor purposes
@@ -81,16 +67,26 @@ class ArmorParent: BaseHullMod() {
             module.fluxTracker.hardFlux = moduleFlux
         }
 
-        // unset hulk for right before any damage gets dealt to the module, this allows for normal processing of hit explosions
+        // Temporarily unset drone so damage effects display properly, then reapply
         override fun reportDamageApplied(source: Any, target: CombatEntityAPI, result: ApplyDamageResultAPI) {
             val module = target as ShipAPI
-            if (module.isHulk && module.hitpoints > 0 && !module.hasTag(MODULE_DEAD)) module.isHulk = false
+            if (module.hasTag(MODULE_HULKED) && module.isDrone && module.hitpoints > 0 && !module.hasTag(MODULE_DEAD)) {
+                module.isDrone = false
+                // Schedule reapplication of drone state for the next frame
+                // This allows damage effects to play properly
+                Global.getCombatEngine().addPlugin(object : com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin() {
+                    override fun advance(amount: Float, events: MutableList<InputEventAPI>?) {
+                        if (module.isDrone == false && module.hitpoints > 0 && !module.hasTag(MODULE_DEAD)) {
+                            module.isDrone = true
+                        }
+                        Global.getCombatEngine().removePlugin(this)
+                    }
+                })
+            }
         }
 
-        // for some reason the above listener doesn't catch when the module is actually going to be dead.
         override fun notifyAboutToTakeHullDamage(param: Any, module: ShipAPI, point: Vector2f, damageAmount: Float): Boolean {
             if (module.hitpoints <= damageAmount && !module.hasTag(MODULE_DEAD)) {
-                module.isHulk = false
                 module.isDrone = false
                 module.addTag(MODULE_DEAD)
             }
